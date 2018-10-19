19c63edfb56a6afb9eb259d5535dc68d8be35b6e
Kevin Davis <dawnpatrol506@gmail.com>
2018-10-19 08:19:23 -0700

    Inventory control button added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('.modal').modal();
     $('select').formSelect();
 
+    
+
     const add = require('./functions/add');
     const ship = require('./functions/ship');
     const firebaseFunctions = require('./functions/firebaseFunctions');
@@ -39,16 +41,16 @@ $(document).ready(function () {
     })
 
     $('#sign-in-button').on('click', (e) => {
-            const provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(provider).then(result => {
-                
-                $('#sign-in').attr('style', 'display:none');
-                $('#nav-area').attr('style', '');
-                $('#main-container').attr('style', '');
-                user = (result.user.displayName);
-            }).catch(err => {
-                if (err) throw err;
-            })
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(result => {
+
+            $('#sign-in').attr('style', 'display:none');
+            $('#nav-area').attr('style', '');
+            $('#main-container').attr('style', '');
+            user = (result.user.displayName);
+        }).catch(err => {
+            if (err) throw err;
+        })
         $('#sign-in-button').off('click');
     });
 
@@ -59,6 +61,33 @@ $(document).ready(function () {
             helper.countActiveParts(snap, color);
         })
     })
+
+    $(document).on('click', '.table-row', function () {
+        if ($(this).hasClass('showing-buttons') || $(this).hasClass('buttons-removed')) {
+            $(this).removeClass('buttons-removed');
+        }
+        else {
+            if($('tooltipped').length){
+                $('tooltipped').tooltip('destroy');
+            }
+            $('.table-row').find('.temp-td').remove();
+            $('.showing-buttons').removeClass('showing-buttons');
+            let countBtn = $('<a class="btn-small waves-effect waves-light green tooltipped black-text" data-position="top" data-tooltip="Correct inventory count"><i class="material-icons">check_circle_outline</i></a>');
+            let reviewBtn = $('<a class="btn-small waves-effect waves-light orange tooltipped black-text" data-position="top" data-tooltip="Request inventory count"><i class="material-icons">radio_button_unchecked</i></a>');
+            let closeBtn = $('<a class="btn-small waves-effect waves-light red td-closer tooltipped black-text" data-position="top" data-tooltip="Hide buttons"><i class="material-icons">cancel</i></a>');
+            let row = $('<td class="temp-td">').append(countBtn, reviewBtn, closeBtn);
+            $(this).append(row);
+            $(this).addClass('showing-buttons');
+            $('.tooltipped').tooltip();
+        }
+    })
+
+    $(document).on('click', '.td-closer', function(){
+        $('.tooltipped').tooltip('destroy');
+        $('.temp-td').remove();
+        $('.showing-buttons').addClass('buttons-removed');
+        $('.showing-buttons').removeClass('showing-buttons');
+    });
 
     firebaseFunctions.populateTable();
 })
